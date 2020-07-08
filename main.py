2000d9295fa53d0de7d2cf62f77c99c2f8d8428b
Nanmuhong Ye <nanmuhongy4341@aurorak12.org>
2020-07-08 15:54:14 +0000

    I made the generators

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from PasswordGenerator import passwordgenerator
 from UsernameGenerator import usernamegenerator
 def menu():
-  
+  while 1 == 1
+    cmd = input("type 1 to make a password, type 2 to make a username and a password, EXIT to exit")
+    if cmd == '1':
+      passwordgenerator()
+    elif cmd == '2':
+      usernamegenerator()
+    elif cmd == 'EXIT':
+      break
+    else:
+      print('Sry, I didn\'understand what do you meant. please try again')
