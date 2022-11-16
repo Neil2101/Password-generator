57db06749575c3bfdf46e8d3a6c52468630cdb39
Neil2101 <66876298+Neil2101@users.noreply.github.com>
2022-11-16 14:07:10 -0700

    made code cleaner
    made it more readable

--- Password V.1/PasswordGenerator.py
+++ Password V.1/PasswordGenerator.py
@@ -1,6 +1,16 @@
 def passwordgenerator():
 	try:
+		
+		#imports
+		#really it's just random module
+		
 		from random import shuffle, randint
+		
+		#create all the variables
+		#a """*pretend there's an enter here*""" randomly is horrible code, so why not turn it into a variable
+		
+		enter = """
+		"""
 		password = []
 		letters = [
 		 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
@@ -10,11 +20,15 @@ def passwordgenerator():
 		]
 
 		numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
+		
+		#all the required input
+		
 		numletters = input("how much letters do you want? Answer: ")
 		numnumbers = input("how much numbers do you want? Answer: ")
 		username = input("what's the accompanying username? Answer: ")
-
+		
+		#makes the amount of letters and numbers that the user selected
+		
 		for j in range(0, int(numnumbers)):
 			shuffle(letters)
 			a = numbers[randint(0, len(numbers) - 1)]
@@ -23,18 +37,27 @@ def passwordgenerator():
 			shuffle(numbers)
 			b = letters[randint(0, len(letters) - 1)]
 			password.append(b)
-
+			
+		#makes it more random
+			
 		shuffle(password)
+		
+		#turns password into a string
+		
 		Password = ''.join(password)
 		with open('Pythonstorage.txt', "a") as myfile:
-			myfile.write("Username: " + str(username) + " Password: " + str(Password) +
-			             """,
-                    """)
+			myfile.write("Username: " + str(username) + " Password: " + str(Password) + enter)
 		print(Password)
 	except Exception as E:
+		
+		#just in case ;)
+		
 		print("sorry, something went wrong: " + str(E))
 	finally:
+		
+		#ALWAYS CLOSE THE FILE AFTER YOU'RE DONE!
+		
 		myfile.close()
 
-
+		#actually runs the file, made this mistake before.
 passwordgenerator()
