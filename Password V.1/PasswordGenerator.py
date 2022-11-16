06a831c36eb3a68b81b9b939350fc40d62d59986
Neil2101 <66876298+Neil2101@users.noreply.github.com>
2022-11-16 09:06:25 -0700

    added more randomness to the password, hope that it doesn't break
    Added 2 mroe shuffles, and a range(1,9) rather than 1,2,3,4,5,6,7,8,9,0

--- Password V.1/PasswordGenerator.py
+++ Password V.1/PasswordGenerator.py
@@ -5,16 +5,18 @@ def passwordgenerator():
     letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
                 "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","T","V","W","X","Y","Z"]
 
-    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    numbers = [range(0,9)]
 
     numletters = input("how much letters do you want? Answer: ")
 
     numnumbers = input("how much numbers do you want? Answer: ")
 
     for j in range(0,int(numnumbers)):
-      a = numbers[randint(0,9)]
+      shuffle(letters)
+      a = numbers[randint(0,len(numbers)-1)]
       password.append(a)
     for k in range(0,int(numletters)):
+      shuffle(numbers)
       b = letters[randint(0,len(letters)-1)]
       password.append(b)
 
